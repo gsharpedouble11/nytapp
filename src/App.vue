85f2d187e665804c72518641f84ed3b70880628b
gsharpedouble11 <92782717+gsharpedouble11@users.noreply.github.com>
2021-10-22 17:38:33 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="card-columns">
+
+        <div class="card p-3"
+          v-for="post in posts" v-bind:key="post.data.title">
+
+          <blockquote class="card-blockquote">
+            <p>
+              {{ post.data.title }}
+            </p>
+            <footer>
+              <small>
+                <a target="_blank"
+                  :href="post.data.url">
+                  Read more on {{ post.data.domain }}
+                </a>
+              </small>
+            </footer>
+          </blockquote>
+
+        </div>
+
+      </div>
+
+      <div class="text-center"
+        v-show="postsLoading">
+          Loading...
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  created () {
+    this.getPosts()
+
+    window.addEventListener('scroll', this.handleScroll)
+  },
+
+  data () {
+    return {
+      posts: [],
+      postsLoading: false,
+      nextPage: null
+    }
+  },
+
+  methods: {
+    getPosts (page) {
+      this.postsLoading = true
+
+      var url = 'https://www.reddit.com/r/all/top.json?limit=30&count=30'
+
+      if (page != null) {
+        url = 'https://www.reddit.com/r/all/top.json?limit=30&count=30&after=' + page
+      }
+
+      axios.get(url)
+        .then(response => {
+          this.posts = this.posts.concat(response.data.data.children)
+
+          this.nextPage = response.data.data.after
+
+          this.postsLoading = false
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+
+    handleScroll () {
+      if (document.body.scrollHeight - window.innerHeight - document.body.scrollTop <= 5) {
+        if (this.nextPage != null) {
+          this.getPosts(this.nextPage)
+        }
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
