b9544f35880f7592931b2cb59b585e0676ad4056
gsharpedouble11 <92782717+gsharpedouble11@users.noreply.github.com>
2021-10-27 15:53:47 +0100

    put posts in component

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,23 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div class="card-columns">
-
-        <div class="card p-3"
-          v-for="post in posts" v-bind:key="post.data.title">
-
-          <blockquote class="card-blockquote">
-            <p>
-              {{ post.data.title }}
-            </p>
-            <footer>
-              <small>
-                <a target="_blank"
-                  :href="post.data.url">
-                  Read more on {{ post.data.domain }}
-                </a>
-              </small>
-            </footer>
-          </blockquote>
-
-        </div>
-
-      </div>
-
-      <div class="text-center"
-        v-show="postsLoading">
-          Loading...
-      </div>
-      <button type="button" class="btn btn-secondary btn-lg btn-block" id="more-posts" v-on:click="morePosts">Load more posts</button>
-    </div>
+    <Nav />
+    <Posts />
   </div>
 </template>
 
-<style scoped>
-#more-posts {
-  margin-bottom: 3rem;
-}
-</style>
-
 <script>
-import axios from 'axios'
+import Nav from './components/Nav.vue'
+import Posts from './components/Posts.vue'
 
 export default {
-  created () {
-    this.getPosts()
-  },
-
-  data () {
-    return {
-      posts: [],
-      postsLoading: false,
-      nextPage: null
-    }
-  },
-
-  methods: {
-    getPosts (page) {
-      this.postsLoading = true
-
-      var url = 'https://www.reddit.com/r/all/top.json?limit=30&count=30'
-
-      if (page != null) {
-        url = 'https://www.reddit.com/r/all/top.json?limit=30&count=30&after=' + page
-      }
-
-      axios.get(url)
-        .then(response => {
-          this.posts = this.posts.concat(response.data.data.children)
-
-          this.nextPage = response.data.data.after
-
-          this.postsLoading = false
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-
-    morePosts () {
-      if (this.nextPage != null) {
-        this.getPosts(this.nextPage)
-      }
-
-      if (this.postsLoading === true) {
-        document.getElementById('more-posts').style.display = 'none'
-      } else {
-        document.getElementById('more-posts').style.display = 'block'
-      }
-    }
+  name: 'App',
+  components: {
+    Nav,
+    Posts
   }
 }
 </script>
+
+<style scoped>
+
+</style>
